5259c702b2f9f29dc5d16e6c548ce28bd469871e
yanghoJI <arnilone@gmail.com>
2018-09-04 13:50:35 +0900

    final code #1

---
+++ final/kin/BiMPM_ensemble/SentenceMatchDataStream.py
@@ -0,0 +1,296 @@
+import numpy as np
+import re
+import os
+import copy
+
+def make_batches(size, batch_size):
+    nb_batch = int(np.ceil(size/float(batch_size)))
+    return [(i*batch_size, min(size, (i+1)*batch_size)) for i in range(0, nb_batch)] # zgwang: starting point of each batch
+
+def pad_2d_vals(in_vals, dim1_size, dim2_size, dtype=np.int32):
+    out_val = np.zeros((dim1_size, dim2_size), dtype=dtype)
+    if dim1_size > len(in_vals): dim1_size = len(in_vals)
+    for i in range(dim1_size):
+        cur_in_vals = in_vals[i]
+        cur_dim2_size = dim2_size
+        if cur_dim2_size > len(cur_in_vals): cur_dim2_size = len(cur_in_vals)
+        out_val[i,:cur_dim2_size] = cur_in_vals[:cur_dim2_size]
+    return out_val
+
+def pad_3d_vals(in_vals, dim1_size, dim2_size, dim3_size, dtype=np.int32):
+    out_val = np.zeros((dim1_size, dim2_size, dim3_size), dtype=dtype)
+    if dim1_size > len(in_vals): dim1_size = len(in_vals)
+    for i in range(dim1_size):
+        in_vals_i = in_vals[i]
+        cur_dim2_size = dim2_size
+        if cur_dim2_size > len(in_vals_i): cur_dim2_size = len(in_vals_i)
+        for j in range(cur_dim2_size):
+            in_vals_ij = in_vals_i[j]
+            cur_dim3_size = dim3_size
+            if cur_dim3_size > len(in_vals_ij): cur_dim3_size = len(in_vals_ij)
+            out_val[i, j, :cur_dim3_size] = in_vals_ij[:cur_dim3_size]
+    return out_val
+
+
+def read_all_instances(inpath, word_vocab=None, label_vocab=None, char_vocab=None, max_sent_length=100,
+                       max_char_per_word=10, isLower=True):
+    instances = []
+    infile = open(inpath, 'rt', encoding = 'utf-8')
+    idx = -1
+    for line in infile:
+        idx += 1
+        line = line.strip()
+        if line.startswith('-'): continue
+        items = re.split("\t", line)
+        label = items[0]
+        sentence1 = items[1].strip()
+        sentence2 = items[2].strip()
+        cur_ID = "{}".format(idx)
+        if len(items)>=4: cur_ID = items[3]
+        if isLower:
+            sentence1 = sentence1.lower()
+            sentence2 = sentence2.lower()
+        if label_vocab is not None:
+            label_id = label_vocab.getIndex(label)
+            if label_id >= label_vocab.vocab_size: label_id = 0
+        else:
+            label_id = int(label)
+        word_idx_1 = word_vocab.to_index_sequence(sentence1)
+        word_idx_2 = word_vocab.to_index_sequence(sentence2)
+        if char_vocab is not None:
+            char_matrix_idx_1 = char_vocab.to_character_matrix(sentence1, max_char_per_word=max_char_per_word)
+            char_matrix_idx_2 = char_vocab.to_character_matrix(sentence2, max_char_per_word=max_char_per_word)
+        else:
+            char_matrix_idx_1 = None
+            char_matrix_idx_2 = None
+        if len(word_idx_1) > max_sent_length:
+            word_idx_1 = word_idx_1[:max_sent_length]
+            if char_vocab is not None: char_matrix_idx_1 = char_matrix_idx_1[:max_sent_length]
+        if len(word_idx_2) > max_sent_length:
+            word_idx_2 = word_idx_2[:max_sent_length]
+            if char_vocab is not None: char_matrix_idx_2 = char_matrix_idx_2[:max_sent_length]
+        instances.append((label, sentence1, sentence2, label_id, word_idx_1, word_idx_2, char_matrix_idx_1, char_matrix_idx_2, cur_ID))
+    infile.close()
+    return instances
+
+def read_all_instances_kin(inpath, word_vocab=None, label_vocab=None, char_vocab=None, max_sent_length=100,
+                       max_char_per_word=10, isLower=True, is_dev = False):
+    if is_dev:
+        data_path = os.path.join(inpath, 'test', 'kin_test_data_2.txt')
+        label_path = os.path.join(inpath, 'test', 'kin_test_label_2.txt')
+    else:
+        data_path = os.path.join(inpath, 'train', 'train_data')
+        label_path = os.path.join(inpath, 'train', 'train_label')
+    instances = []
+    #infile = open(inpath, 'rt', encoding = 'utf-8')
+    data_infile_f = open(data_path, 'rt', encoding='utf-8')
+    data_infile = data_infile_f.readlines()
+    label_infile_f = open(label_path, 'rt', encoding='utf-8')
+    label_infile = label_infile_f.readlines()
+    if len(data_infile) != len(label_infile):
+        print('data length unmatched')
+    idx = -1
+    for i in range(len(label_infile)):
+        idx += 1
+        line = data_infile[i].strip()
+        if line.startswith('-'): continue
+        items = re.split("\t", line)
+        #items = line.split('    ')
+        label = label_infile[i][0].strip()
+        sentence1 = items[0].strip()
+        sentence2 = items[1].strip()
+        cur_ID = "{}".format(idx)
+        if len(items)>=4: cur_ID = items[3]
+        #if isLower:
+        #    sentence1 = sentence1.lower()
+        #    sentence2 = sentence2.lower()
+        if label_vocab is not None:
+            label_id = label_vocab.getIndex(label)
+            if label_id >= label_vocab.vocab_size: label_id = 0
+        else:
+            label_id = int(label)
+        word_idx_1 = word_vocab.to_index_sequence(sentence1)
+        word_idx_2 = word_vocab.to_index_sequence(sentence2)
+        if char_vocab is not None:
+            char_matrix_idx_1 = char_vocab.to_character_matrix(sentence1, max_char_per_word=max_char_per_word)
+            char_matrix_idx_2 = char_vocab.to_character_matrix(sentence2, max_char_per_word=max_char_per_word)
+        else:
+            char_matrix_idx_1 = None
+            char_matrix_idx_2 = None
+        if len(word_idx_1) > max_sent_length:
+            word_idx_1 = word_idx_1[:max_sent_length]
+            if char_vocab is not None: char_matrix_idx_1 = char_matrix_idx_1[:max_sent_length]
+        if len(word_idx_2) > max_sent_length:
+            word_idx_2 = word_idx_2[:max_sent_length]
+            if char_vocab is not None: char_matrix_idx_2 = char_matrix_idx_2[:max_sent_length]
+        instances.append((label, sentence1, sentence2, label_id, word_idx_1, word_idx_2, char_matrix_idx_1, char_matrix_idx_2, cur_ID))
+    data_infile_f.close()
+    label_infile_f.close()
+    return instances
+
+def read_all_instances_kin_rawdata(rawdata, word_vocab=None, label_vocab=None, char_vocab=None, max_sent_length=100,
+                       max_char_per_word=10, isLower=True, is_dev = False):
+    data_infile = rawdata
+    instances = []
+
+    idx = -1
+    for i in range(len(data_infile)):
+
+        idx += 1
+        line = data_infile[i].strip()
+        #if line.startswith('-'): continue
+        items = re.split("\t", line)
+        sentence1 = items[0].strip()
+        sentence2 = items[1].strip()
+        cur_ID = "{}".format(idx)
+
+        word_idx_1 = word_vocab.to_index_sequence(sentence1)
+        word_idx_2 = word_vocab.to_index_sequence(sentence2)
+
+        if char_vocab is not None:
+            char_matrix_idx_1 = char_vocab.to_character_matrix(sentence1, max_char_per_word=max_char_per_word)
+            char_matrix_idx_2 = char_vocab.to_character_matrix(sentence2, max_char_per_word=max_char_per_word)
+        else:
+            char_matrix_idx_1 = None
+            char_matrix_idx_2 = None
+
+
+        if len(word_idx_1) > max_sent_length:
+            word_idx_1 = word_idx_1[:max_sent_length]
+            if char_vocab is not None: char_matrix_idx_1 = char_matrix_idx_1[:max_sent_length]
+        if len(word_idx_2) > max_sent_length:
+            word_idx_2 = word_idx_2[:max_sent_length]
+            if char_vocab is not None: char_matrix_idx_2 = char_matrix_idx_2[:max_sent_length]
+
+        instances.append((0, sentence1, sentence2, 0, word_idx_1, word_idx_2, char_matrix_idx_1, char_matrix_idx_2, cur_ID))
+
+
+    if len(data_infile) != len(instances):
+        raise notmatched
+    return instances
+
+
+class SentenceMatchDataStream(object):
+    def __init__(self, inpath, word_vocab=None, char_vocab=None, label_vocab=None,
+                 isShuffle=False, isLoop=False, isSort=False, options=None, is_dev=False, raw_data = False, bagging=None):
+        if raw_data:
+            instances = read_all_instances_kin_rawdata(inpath, word_vocab=word_vocab, label_vocab=label_vocab,
+                                               char_vocab=char_vocab, max_sent_length=options.max_sent_length,
+                                               max_char_per_word=options.max_char_per_word,
+                                               isLower=True, is_dev=is_dev)
+        else:
+            instances = read_all_instances_kin(inpath, word_vocab=word_vocab, label_vocab=label_vocab,
+                        char_vocab=char_vocab, max_sent_length=options.max_sent_length, max_char_per_word=options.max_char_per_word,
+                                           isLower=True, is_dev=is_dev)
+
+
+        # sort instances based on sentence length
+        if isSort: instances = sorted(instances, key=lambda instance: (len(instance[4]), len(instance[5]))) # sort instances based on length
+        self.num_instances = len(instances)
+        
+        # distribute into different buckets
+        batch_spans = make_batches(self.num_instances, options.batch_size)
+        self.batches = []
+        for batch_index, (batch_start, batch_end) in enumerate(batch_spans):
+            cur_instances = []
+            for i in range(batch_start, batch_end):
+                cur_instances.append(instances[i])
+            cur_batch = InstanceBatch(cur_instances, with_char=options.with_char)
+            self.batches.append(cur_batch)
+
+        #raise sign1
+
+        instances = None
+        self.num_batch = len(self.batches)
+        self.index_array = np.arange(self.num_batch)
+        self.isShuffle = isShuffle
+        if self.isShuffle: np.random.shuffle(self.index_array)
+        if bagging != None:
+            print('Apply bagging... rate = {}'.format(bagging))
+            num_bat = int(self.num_batch * bagging)
+            self.index_array = self.index_array[:num_bat]
+            self.num_batch = num_bat
+            if len(self.index_array) !=self.num_batch:
+                raise lengthNotMatched
+
+        self.isLoop = isLoop
+        self.cur_pointer = 0
+    
+    def nextBatch(self):
+        if self.cur_pointer>=self.num_batch:
+            if not self.isLoop: return None
+            self.cur_pointer = 0 
+            if self.isShuffle: np.random.shuffle(self.index_array) 
+#         print('{} '.format(self.index_array[self.cur_pointer]))
+        cur_batch = self.batches[self.index_array[self.cur_pointer]]
+        self.cur_pointer += 1
+        return cur_batch
+
+    def shuffle(self):
+        if self.isShuffle: np.random.shuffle(self.index_array)
+
+    def reset(self):
+        self.cur_pointer = 0
+    
+    def get_num_batch(self):
+        return self.num_batch
+
+    def get_num_instance(self):
+        return self.num_instances
+
+    def get_batch(self, i):
+        if i >= self.num_batch: return None
+        return self.batches[self.index_array[i]]
+
+
+class InstanceBatch(object):
+    def __init__(self, instances, with_char=False):
+        self.instances = instances
+        self.batch_size = len(instances)
+        self.question_len = 0
+        self.passage_len = 0
+
+        self.question_lengths = []  # tf.placeholder(tf.int32, [None])
+        self.in_question_words = []  # tf.placeholder(tf.int32, [None, None]) # [batch_size, question_len]
+        self.passage_lengths = []  # tf.placeholder(tf.int32, [None])
+        self.in_passage_words = []  # tf.placeholder(tf.int32, [None, None]) # [batch_size, passage_len]
+        self.label_truth = []  # [batch_size]
+
+        if with_char:
+            self.in_question_chars = [] # tf.placeholder(tf.int32, [None, None, None])  # [batch_size, question_len, q_char_len]
+            self.question_char_lengths = [] # tf.placeholder(tf.int32, [None, None])  # [batch_size, question_len]
+            self.in_passage_chars = [] # tf.placeholder(tf.int32, [None, None, None])  # [batch_size, passage_len, p_char_len]
+            self.passage_char_lengths = [] # tf.placeholder(tf.int32, [None, None])  # [batch_size, passage_len]
+
+        for (label, sentence1, sentence2, label_id, word_idx_1, word_idx_2, char_matrix_idx_1, char_matrix_idx_2, cur_ID) in instances:
+            cur_question_length = len(word_idx_1)
+            cur_passage_length = len(word_idx_2)
+            if self.question_len < cur_question_length: self.question_len = cur_question_length
+            if self.passage_len < cur_passage_length: self.passage_len = cur_passage_length
+            self.question_lengths.append(cur_question_length)
+            self.in_question_words.append(word_idx_1)
+            self.passage_lengths.append(cur_passage_length)
+            self.in_passage_words.append(word_idx_2)
+            self.label_truth.append(label_id)
+            if with_char:
+                self.in_question_chars.append(char_matrix_idx_1)
+                self.in_passage_chars.append(char_matrix_idx_2)
+                self.question_char_lengths.append([len(cur_char_idx) for cur_char_idx in char_matrix_idx_1])
+                self.passage_char_lengths.append([len(cur_char_idx) for cur_char_idx in char_matrix_idx_2])
+
+        # padding all value into np arrays
+        self.question_lengths = np.array(self.question_lengths, dtype=np.int32)
+        self.in_question_words = pad_2d_vals(self.in_question_words, self.batch_size, self.question_len, dtype=np.int32)
+        self.passage_lengths = np.array(self.passage_lengths, dtype=np.int32)
+        self.in_passage_words = pad_2d_vals(self.in_passage_words, self.batch_size, self.passage_len, dtype=np.int32)
+        self.label_truth = np.array(self.label_truth, dtype=np.int32)
+        if with_char:
+            max_char_length1 = np.max([np.max(aa) for aa in self.question_char_lengths])
+            self.in_question_chars = pad_3d_vals(self.in_question_chars, self.batch_size,  self.question_len,
+                                                   max_char_length1, dtype=np.int32)
+            max_char_length2 = np.max([np.max(aa) for aa in self.passage_char_lengths])
+            self.in_passage_chars = pad_3d_vals(self.in_passage_chars, self.batch_size,  self.passage_len,
+                                                max_char_length2, dtype=np.int32)
+
+            self.question_char_lengths = pad_2d_vals(self.question_char_lengths, self.batch_size,  self.question_len)
+            self.passage_char_lengths = pad_2d_vals(self.passage_char_lengths, self.batch_size,  self.passage_len)
---
+++ final/kin/BiMPM_ensemble/SentenceMatchModelGraph.py
@@ -0,0 +1,221 @@
+import tensorflow as tf
+import layer_utils
+import match_utils
+
+
+class SentenceMatchModelGraph(object):
+    def __init__(self, num_classes, word_vocab=None, char_vocab=None, is_training=True, options=None, global_step=None):
+        self.options = options
+        self.create_placeholders()
+        self.create_model_graph(num_classes, word_vocab, char_vocab, is_training, global_step=global_step)
+
+    def create_placeholders(self):
+        self.question_lengths = tf.placeholder(tf.int32, [None])
+        self.passage_lengths = tf.placeholder(tf.int32, [None])
+        self.truth = tf.placeholder(tf.int32, [None]) # [batch_size]
+        self.in_question_words = tf.placeholder(tf.int32, [None, None]) # [batch_size, question_len]
+        self.in_passage_words = tf.placeholder(tf.int32, [None, None]) # [batch_size, passage_len]
+
+        if self.options.with_char:
+            self.question_char_lengths = tf.placeholder(tf.int32, [None,None]) # [batch_size, question_len]
+            self.passage_char_lengths = tf.placeholder(tf.int32, [None,None]) # [batch_size, passage_len]
+            self.in_question_chars = tf.placeholder(tf.int32, [None, None, None]) # [batch_size, question_len, q_char_len]
+            self.in_passage_chars = tf.placeholder(tf.int32, [None, None, None]) # [batch_size, passage_len, p_char_len]
+
+    def create_feed_dict(self, cur_batch, is_training=False):
+        feed_dict = {
+            self.question_lengths: cur_batch.question_lengths,
+            self.passage_lengths: cur_batch.passage_lengths,
+            self.in_question_words: cur_batch.in_question_words,
+            self.in_passage_words: cur_batch.in_passage_words,
+            self.truth : cur_batch.label_truth,
+        }
+
+        if self.options.with_char:
+            feed_dict[self.question_char_lengths] = cur_batch.question_char_lengths
+            feed_dict[self.passage_char_lengths] = cur_batch.passage_char_lengths
+            feed_dict[self.in_question_chars] = cur_batch.in_question_chars
+            feed_dict[self.in_passage_chars] = cur_batch.in_passage_chars
+
+        return feed_dict
+
+
+    def create_model_graph(self, num_classes, word_vocab=None, char_vocab=None, is_training=True, global_step=None):
+        options = self.options
+        # ======word representation layer======
+        in_question_repres = []
+        in_passage_repres = []
+        input_dim = 0
+
+        if word_vocab == []:
+            word_vocab_triger = True
+            word_vocab = 1
+        else:
+            word_vocab_triger = False
+
+        if word_vocab is not None:
+            word_vec_trainable = True
+            cur_device = '/gpu:0'
+            if options.fix_word_vec:
+                word_vec_trainable = False
+                cur_device = '/cpu:0'
+            with tf.device(cur_device):
+                if word_vocab_triger == True:
+
+                    self.word_embedding = tf.get_variable("word_embedding", [879129, options.word_emb_dim], trainable=word_vec_trainable,
+                                                          dtype=tf.float32)
+                    ##real = 127499   dummy = 211
+                    ##real = 879129   dummy = 211
+
+                    ##real = 86780   dummy = 211
+                else:
+                    self.word_embedding = tf.get_variable("word_embedding", trainable=word_vec_trainable,
+                                                    initializer=tf.constant(word_vocab.word_vecs), dtype=tf.float32)
+                    print('word emb size cheak = real = {}\tpredict = {}'.format(word_vocab.word_vecs.shape, [879129, options.word_emb_dim]))
+
+            in_question_word_repres = tf.nn.embedding_lookup(self.word_embedding, self.in_question_words) # [batch_size, question_len, word_dim]
+            in_passage_word_repres = tf.nn.embedding_lookup(self.word_embedding, self.in_passage_words) # [batch_size, passage_len, word_dim]
+            in_question_repres.append(in_question_word_repres)
+            in_passage_repres.append(in_passage_word_repres)
+
+            input_shape = tf.shape(self.in_question_words)
+            batch_size = input_shape[0]
+            question_len = input_shape[1]
+            input_shape = tf.shape(self.in_passage_words)
+            passage_len = input_shape[1]
+            if word_vocab_triger == True:
+                input_dim+= options.word_emb_dim
+            else:
+                input_dim += word_vocab.word_dim
+
+
+        if char_vocab == []:
+            char_vocab_triger = True
+            char_vocab = 1
+        else:
+            char_vocab_triger = False
+
+        if options.with_char and char_vocab is not None:
+            input_shape = tf.shape(self.in_question_chars)
+            batch_size = input_shape[0]
+            question_len = input_shape[1]
+            q_char_len = input_shape[2]
+            input_shape = tf.shape(self.in_passage_chars)
+            passage_len = input_shape[1]
+            p_char_len = input_shape[2]
+            char_dim = self.options.char_emb_dim
+
+            #options.use_cudnn = True
+            if char_vocab_triger ==True:
+                self.char_embedding = tf.get_variable("char_embedding", [2757, char_dim], dtype=tf.float32)
+                ##real = 2757   dummy = 224
+                ##real = 2157   dummy = 224
+            else:
+                self.char_embedding = tf.get_variable("char_embedding", initializer=tf.constant(char_vocab.word_vecs), dtype=tf.float32)
+                print('word emb size cheak = real = {}\tpredict = {}'.format(char_vocab.word_vecs.shape, [2757, char_dim]))
+
+            in_question_char_repres = tf.nn.embedding_lookup(self.char_embedding, self.in_question_chars) # [batch_size, question_len, q_char_len, char_dim]
+            in_question_char_repres = tf.reshape(in_question_char_repres, shape=[-1, q_char_len, char_dim])
+            question_char_lengths = tf.reshape(self.question_char_lengths, [-1])
+            quesiton_char_mask = tf.sequence_mask(question_char_lengths, q_char_len, dtype=tf.float32)  # [batch_size*question_len, q_char_len]
+            in_question_char_repres = tf.multiply(in_question_char_repres, tf.expand_dims(quesiton_char_mask, axis=-1))
+
+
+            in_passage_char_repres = tf.nn.embedding_lookup(self.char_embedding, self.in_passage_chars) # [batch_size, passage_len, p_char_len, char_dim]
+            in_passage_char_repres = tf.reshape(in_passage_char_repres, shape=[-1, p_char_len, char_dim])
+            passage_char_lengths = tf.reshape(self.passage_char_lengths, [-1])
+            passage_char_mask = tf.sequence_mask(passage_char_lengths, p_char_len, dtype=tf.float32)  # [batch_size*passage_len, p_char_len]
+            in_passage_char_repres = tf.multiply(in_passage_char_repres, tf.expand_dims(passage_char_mask, axis=-1))
+
+            (question_char_outputs_fw, question_char_outputs_bw, _) = layer_utils.my_lstm_layer(in_question_char_repres, options.char_lstm_dim,
+                    input_lengths=question_char_lengths,scope_name="char_lstm", reuse=False,
+                    is_training=is_training, dropout_rate=options.dropout_rate, use_cudnn=options.use_cudnn)
+            question_char_outputs_fw = layer_utils.collect_final_step_of_lstm(question_char_outputs_fw, question_char_lengths - 1)
+            question_char_outputs_bw = question_char_outputs_bw[:, 0, :]
+            question_char_outputs = tf.concat(axis=1, values=[question_char_outputs_fw, question_char_outputs_bw])
+            question_char_outputs = tf.reshape(question_char_outputs, [batch_size, question_len, 2*options.char_lstm_dim])
+
+            (passage_char_outputs_fw, passage_char_outputs_bw, _) = layer_utils.my_lstm_layer(in_passage_char_repres, options.char_lstm_dim,
+                    input_lengths=passage_char_lengths, scope_name="char_lstm", reuse=True,
+                    is_training=is_training, dropout_rate=options.dropout_rate, use_cudnn=options.use_cudnn)
+            passage_char_outputs_fw = layer_utils.collect_final_step_of_lstm(passage_char_outputs_fw, passage_char_lengths - 1)
+            passage_char_outputs_bw = passage_char_outputs_bw[:, 0, :]
+            passage_char_outputs = tf.concat(axis=1, values=[passage_char_outputs_fw, passage_char_outputs_bw])
+            passage_char_outputs = tf.reshape(passage_char_outputs, [batch_size, passage_len, 2*options.char_lstm_dim])
+                
+            in_question_repres.append(question_char_outputs)
+            in_passage_repres.append(passage_char_outputs)
+
+            input_dim += 2*options.char_lstm_dim
+
+        in_question_repres = tf.concat(axis=2, values=in_question_repres) # [batch_size, question_len, dim]
+        in_passage_repres = tf.concat(axis=2, values=in_passage_repres) # [batch_size, passage_len, dim]
+
+        if is_training:
+            in_question_repres = tf.nn.dropout(in_question_repres, (1 - options.dropout_rate))
+            in_passage_repres = tf.nn.dropout(in_passage_repres, (1 - options.dropout_rate))
+
+        mask = tf.sequence_mask(self.passage_lengths, passage_len, dtype=tf.float32) # [batch_size, passage_len]
+        question_mask = tf.sequence_mask(self.question_lengths, question_len, dtype=tf.float32) # [batch_size, question_len]
+
+        # ======Highway layer======
+        if options.with_highway:
+            with tf.variable_scope("input_highway"):
+                in_question_repres = match_utils.multi_highway_layer(in_question_repres, input_dim, options.highway_layer_num)
+                tf.get_variable_scope().reuse_variables()
+                in_passage_repres = match_utils.multi_highway_layer(in_passage_repres, input_dim, options.highway_layer_num)
+
+        # in_question_repres = tf.multiply(in_question_repres, tf.expand_dims(question_mask, axis=-1))
+        # in_passage_repres = tf.multiply(in_passage_repres, tf.expand_dims(mask, axis=-1))
+
+        # ========Bilateral Matching=====
+        (match_representation, match_dim) = match_utils.bilateral_match_func(in_question_repres, in_passage_repres,
+                        self.question_lengths, self.passage_lengths, question_mask, mask, input_dim, is_training, options=options)
+
+        #========Prediction Layer=========
+        # match_dim = 4 * self.options.aggregation_lstm_dim
+        w_0 = tf.get_variable("w_0", [match_dim, match_dim/2], dtype=tf.float32)
+        b_0 = tf.get_variable("b_0", [match_dim/2], dtype=tf.float32)
+        w_1 = tf.get_variable("w_1", [match_dim/2, num_classes],dtype=tf.float32)
+        b_1 = tf.get_variable("b_1", [num_classes],dtype=tf.float32)
+
+        # if is_training: match_representation = tf.nn.dropout(match_representation, (1 - options.dropout_rate))
+        logits = tf.matmul(match_representation, w_0) + b_0
+        logits = tf.tanh(logits)
+        if is_training: logits = tf.nn.dropout(logits, (1 - options.dropout_rate))
+        logits = tf.matmul(logits, w_1) + b_1
+
+        self.prob = tf.nn.softmax(logits)
+        
+        gold_matrix = tf.one_hot(self.truth, num_classes, dtype=tf.float32)
+        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=gold_matrix))
+
+        correct = tf.nn.in_top_k(logits, self.truth, 1)
+        self.eval_correct = tf.reduce_sum(tf.cast(correct, tf.int32))
+        self.predictions = tf.argmax(self.prob, 1)
+
+        if not is_training: return
+
+        tvars = tf.trainable_variables()
+        if self.options.lambda_l2>0.0:
+            l2_loss = tf.add_n([tf.nn.l2_loss(v) for v in tvars if v.get_shape().ndims > 1])
+            self.loss = self.loss + self.options.lambda_l2 * l2_loss
+
+        if self.options.optimize_type == 'adadelta':
+            optimizer = tf.train.AdadeltaOptimizer(learning_rate=self.options.learning_rate)
+        elif self.options.optimize_type == 'adam':
+            optimizer = tf.train.AdamOptimizer(learning_rate=self.options.learning_rate)
+
+        grads = layer_utils.compute_gradients(self.loss, tvars)
+        grads, _ = tf.clip_by_global_norm(grads, self.options.grad_clipper)
+        self.train_op = optimizer.apply_gradients(zip(grads, tvars), global_step=global_step)
+        # self.train_op = optimizer.apply_gradients(zip(grads, tvars))
+
+        if self.options.with_moving_average:
+            # Track the moving averages of all trainable variables.
+            MOVING_AVERAGE_DECAY = 0.9999  # The decay to use for the moving average.
+            variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+            variables_averages_op = variable_averages.apply(tf.trainable_variables())
+            train_ops = [self.train_op, variables_averages_op]
+            self.train_op = tf.group(*train_ops)
+
